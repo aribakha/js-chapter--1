a3e1613ed5c4b9992a1664a8f1ac3e8993abac7f
aribakha <140427904+aribakha@users.noreply.github.com>
2023-10-28 20:00:16 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,34 @@
+                                   // CHAPTER  # 1 "ARRAY"
+                                     //  QUESTION # 1
+
+// alert("Welcome")
+
+                                     //  QUESTION # 2
+
+// alert("Error!Please enter a valid password")     
+
+                                     //  QUESTION # 3
+                                     
+// alert("Welcome to Js land.... \n Happy Coding!")
+
+                                     //  QUESTION # 4
+                                     
+// alert("Welcome to JS Land..")
+ 
+                                     //  QUESTION # 5
+                                    
+// alert("Hello..I can run JS through my web browser's console")
+// console.log("Hello..I can run JS through my web browser's console")
+   
+                                     //  QUESTION # 6
+
+//  var showAlert="Hello..!Dear"
+//  alert("showAlert")                                
+                                    
+
+
+
+
+
+
+
